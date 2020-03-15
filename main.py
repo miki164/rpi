53171c097c1e75fb64ba48b55b1ddc56e225b765
miki164 <miki3867@gmail.com>
2020-03-15 16:42:08 +0100

    faxf

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from gpiozero import TonalBuzzer
-from gpiozero.tones import Tone
-from gpiozero.tools import sin_values
-from time import sleep
+from gpiozero import Buzzer
 
-tb = TonalBuzzer(20)
-for note in 'C4 D4 E4 F4 G4 A4 B4 C5'.split():
-    tone = Tone(note)
-    print(repr(tone))
-    tb.play(tone)
-    sleep(0.3)
-
-tb.stop()
+bz = Buzzer(3)
+bz.on()
