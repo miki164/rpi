b55e9f61387714e1cb88b7ffc2a58de248350efa
Miki <miki3867@gmail.com>
2020-03-17 18:30:10 +0000

    Tasks

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import asyncio
 buzzer = RpiBuzzer()
 led = LedOutput()
 
-executor = ProcessPoolExecutor(2)
-loop = asyncio.get_event_loop()
-bz = asyncio.create_task(loop.run_in_executor(executor, buzzer.boo_beep))
-l = asyncio.create_task(loop.run_in_executor(executor, led.blink))
+if __name__=="__main__": 
+    executor = ProcessPoolExecutor(2)
+    loop = asyncio.get_event_loop()
+    tasks = [loop.create_task(led.blink(20)),loop.create_task(buzzer.boo_beep())]
+    
+    loop.run_until_complete(asyncio.wait(tasks))
+    
+    loop.close()
+
