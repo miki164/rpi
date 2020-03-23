87546a3b85282ea7ec2f450c90d586f6e5181216
miki164 <miki3867@gmail.com>
2020-03-23 18:13:34 +0100

    Bugfix

--- core/lcd.py
+++ core/lcd.py
@@ -5,6 +5,6 @@ class LCD:
     def __init__(self):
         self.lcd = CharLCD('PCF8574', 0x27)
 
-    def print_something(self):
+    async def print_something(self):
         self.lcd.write_string('NIE NAWIDZĘ LUDZI KURWA MAĆ')
 
