05e9a75db18ff83eef9d45054f0e937b20180a83
miki164 <miki3867@gmail.com>
2020-03-28 15:45:47 +0100

    Shitty code, in shitty language.

--- server/api/routes/API.js
+++ server/api/routes/API.js
@@ -3,27 +3,37 @@ var router = express.Router();
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+//Save your life, don't look at it.
 router.get("/", function(req, res, next) {
     let a = url => {
-        if(req.params) {
-            axios.get(url)
-                .then(response => {
-                    const $ = cheerio.load(response['data']);
-                    let content = "";
-                    let j = 0;
-                    $('table').eq(1).find('table')
-                        .find('a').each((i, elem) => {
-                            let href = $(elem).attr('href').replace("/?s=", "/");
-                            console.log(href);
-                            if(href.startsWith("/s")) {
-                                j+=1;
-                                content += i+1 + ".<a href=\""+href+"\">"+$(elem).text()+"</a></br>"
-                            }
-                        }).html();
-                    res.send(content);
-                });
-        }
+        axios.get(url)
+            .then(response => {
+                const $ = cheerio.load(response['data']);
+                let content = "<table>" +
+                    "<tr><th>Name</th><th>noradid</th><th>Period[min]</th><th>Magnitude</th></tr>";
+
+                $('table').eq(1)
+                    .find('table')
+                    .find('tr')
+                    .each((i, elem) => {
+                        const tds = $(elem).find('td');
+                        const name = tds.eq(0).text();
+                        const noradid = tds.eq(1).text();
+                        const period = tds.eq(4).text();
+                        const magnitude = tds.eq(5).text();
+                        content += "<tr>"
+                            + "<th><a href=\"satellite/"+noradid+"\">" + name+"</a></th>"
+                            + "<th>" + noradid + "</th>" +
+                            + "<th>" + period + "</th>"
+                            + "<th>" + magnitude+ "</th>"
+                            +"</tr>"
+                    });
+
+                content+="</table>";
+                res.send(content);
+            });
     };
+
     a("https://www.n2yo.com/satellites/?c=1");
 });
 
